67ff75128e2f83fc69011f2d7515aedb7b2fee36
Marie Chow <chowma3@oocl.com>
2022-12-05 16:09:50 +0800

    refactor: remove duplicated code

--- src/main/java/com/afs/refactoring/Police.java
+++ src/main/java/com/afs/refactoring/Police.java
@@ -5,7 +5,6 @@ public class Police {
     public static final int LEGAL_AGE = 18;
 
     public boolean checkDriver(Driver driver) {
-        if(driver.age >= LEGAL_AGE) return true;
-        else return false;
+        return driver.age >= LEGAL_AGE;
     }
 }
